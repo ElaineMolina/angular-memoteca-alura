990aa31398010acade77c88be0cbeab898f577df
ElaineMolina <elainemolina10@gmail.com>
2023-07-17 16:37:34 -0300

    2 Entendendo o fluxo de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CreateThoughtComponent } from './components/thoughts/create-thought/cre
     CreateThoughtComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-thought',
   templateUrl: './create-thought.component.html',
-  styleUrls: ['./create-thought.component.css']
+  styleUrls: ['./create-thought.component.css'],
 })
 export class CreateThoughtComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  criarPensamento(){
+    alert('Novo pensamento criado!');
   }
-
+  cancelar(){
+    alert('Ação cancelada!');
+  }
 }
